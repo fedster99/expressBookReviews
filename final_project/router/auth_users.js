40e5d4a1263aacc90d5e8fe07e8b43c5076a0774
federicoaltepost <federico.altepost@icloud.com>
2023-03-08 20:24:06 -1000

    latest version

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,25 +5,105 @@ const regd_users = express.Router();
 
 let users = [];
 
+// code to check is the username is valid
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+    return users.some(u => u.username === username);
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+
+// code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password) => {
+    const user = users.find(u => u.username === username);
+    if (!user) {
+      return false;
+    }
+    return user.password === password;
+  };
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (!username || !password) {
+      return res.status(400).json({ message: 'Username or password not provided' });
+    }
+    if (!isValid(username)) {
+      return res.status(400).json({ message: 'Username is not valid' });
+    }
+    if (!authenticatedUser(username, password)) {
+      return res.status(400).json({ message: 'Username or password is incorrect' });
+    }
+  
+    // Create the JWT token
+    const token = jwt.sign({ username }, process.env.SECRET_KEY);
+    return res.status(200).json({ token });
+  });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    const isbn = req.params.isbn;
+    const review = req.query.review;
+    const username = req.query.username;
+  
+    if (!review) {
+      return res.status(400).json({ message: 'Review not provided' });
+    }
+  
+    if (!username) {
+      return res.status(400).json({ message: 'Username not provided' });
+    }
+  
+    if (!isValid(username)) {
+      return res.status(400).json({ message: 'Username not valid' });
+    }
+  
+    for (const key in books) {
+      const book = books[key];
+      if (book.isbn === isbn) {
+        const existingReview = book.reviews.find(r => r.username === username);
+        if (existingReview) {
+          existingReview.review = review;
+          return res.status(200).json({ message: 'Review Updated!' });
+        } else {
+          book.reviews.push({ username, review });
+          return res.status(200).json({ message: 'Review Added!' });
+        }
+      }
+    }
+    return res.status(404).json({ message: 'Book not found' });
+  });
+
+
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.query.username;
+  
+    if (!username) {
+      return res.status(400).json({ message: 'Username not provided' });
+    }
+  
+    if (!isValid(username)) {
+      return res.status(400).json({ message: 'Username not valid' });
+    }
+  
+    for (const key in books) {
+      const book = books[key];
+      if (book.isbn === isbn) {
+        const reviewIndex = book.reviews.findIndex(r => r.username === username);
+        if (reviewIndex !== -1) {
+          book.reviews.splice(reviewIndex, 1);
+          return res.status(200).json({ message: 'Review Deleted!' });
+        } else {
+          return res.status(404).json({ message: 'No reviews found for the user' });
+        }
+      }
+    }
+    return res.status(404).json({ message: 'Book not found' });
+  });
+
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,63 +20,144 @@ public_users.post('/register', (req, res) => {
   
 
 // Get the list of books available in the shop
-public_users.get('/', (req, res) => {
-    return res.status(200).json(Object.values(books));
+// public_users.get('/', (req, res) => {
+//    return res.status(200).json(Object.values(books));
+// });
+
+// task 10 version
+public_users.get('/', async (req, res) => {
+    try {
+      const response = await axios.get('booksdb.js');
+      res.status(200).json(response.data);
+    } catch (err) {
+      res.status(400).json({ message: 'Something went wrong' });
+    }
   });
   
 
 // Get book details based on ISBN
+//public_users.get('/isbn/:isbn', (req, res) => {
+//    const isbn = req.params.isbn;
+//    const book = Object.values(books).find(b => b.isbn === isbn);
+//    if (book) {
+//     return res.status(200).json(book);
+//    } else {
+//      return res.status(404).json({ message: 'Book not found' });
+//    }
+//  });
+
+
+//task 11 version
+
 public_users.get('/isbn/:isbn', (req, res) => {
     const isbn = req.params.isbn;
-    const book = Object.values(books).find(b => b.isbn === isbn);
-    if (book) {
-      return res.status(200).json(book);
-    } else {
-      return res.status(404).json({ message: 'Book not found' });
-    }
+    axios.get('booksdb.js')
+    .then(function(response){
+      const book = response.data.find(b => b.isbn === isbn);
+      if (book) {
+        return res.status(200).json(book);
+      } else {
+        return res.status(404).json({ message: 'Book not found' });
+      }
+    })
+    .catch(function(error){
+      console.log(error);
+    });
   });
   
   
 // Get book details based on author
-public_users.get('/author/:author', (req, res) => {
+//public_users.get('/author/:author', (req, res) => {
+//    const author = req.params.author;
+//    const bookKeys = Object.keys(books);
+//    const matchingBooks = [];
+  
+//    for (const key of bookKeys) {
+//      const book = books[key];
+//      if (book.author === author) {
+//        matchingBooks.push(book);
+//      }
+//    }
+  
+//    if (matchingBooks.length === 0) {
+//      return res.status(404).json({ message: 'No books found for the author' });
+//    }
+  
+//    return res.status(200).json(matchingBooks);
+//  });
+
+
+
+// TASK 12 - Get book details based on author with async-await
+public_users.get('/author/:author', async (req, res) => {
     const author = req.params.author;
-    const bookKeys = Object.keys(books);
-    const matchingBooks = [];
-  
-    for (const key of bookKeys) {
-      const book = books[key];
-      if (book.author === author) {
-        matchingBooks.push(book);
-      }
+    try {
+        const response = await axios.get('booksdb.js');
+        const bookKeys = Object.keys(response.data);
+        const matchingBooks = [];
+    
+        for (const key of bookKeys) {
+          const book = response.data[key];
+          if (book.author === author) {
+            matchingBooks.push(book);
+          }
+        }
+    
+        if (matchingBooks.length === 0) {
+          return res.status(404).json({ message: 'No books found for the author' });
+        }
+    
+        return res.status(200).json(matchingBooks);
+    } catch (error) {
+        console.log(error);
     }
-  
-    if (matchingBooks.length === 0) {
-      return res.status(404).json({ message: 'No books found for the author' });
-    }
-  
-    return res.status(200).json(matchingBooks);
-  });
+});
   
 
 // Get all books based on title
+//public_users.get('/title/:title', (req, res) => {
+//    const title = req.params.title;
+//    const bookKeys = Object.keys(books);
+//    const matchingBooks = [];
+  
+//    for (const key of bookKeys) {
+//      const book = books[key];
+//      if (book.title === title) {
+//        matchingBooks.push(book);
+//      }
+//    }
+  
+//    if (matchingBooks.length === 0) {
+//      return res.status(404).json({ message: 'No books found with the given title' });
+ //   }
+  
+//    return res.status(200).json(matchingBooks);
+//  });
+
+// Task 13
 public_users.get('/title/:title', (req, res) => {
     const title = req.params.title;
-    const bookKeys = Object.keys(books);
-    const matchingBooks = [];
+    axios.get('booksdb.js')
+    .then(response => {
+      const bookKeys = Object.keys(response.data);
+      const matchingBooks = [];
   
-    for (const key of bookKeys) {
-      const book = books[key];
-      if (book.title === title) {
-        matchingBooks.push(book);
+      for (const key of bookKeys) {
+        const book = response.data[key];
+        if (book.title === title) {
+          matchingBooks.push(book);
+        }
       }
-    }
   
-    if (matchingBooks.length === 0) {
-      return res.status(404).json({ message: 'No books found with the given title' });
-    }
+      if (matchingBooks.length === 0) {
+        return res.status(404).json({ message: 'No books found with the given title' });
+      }
   
-    return res.status(200).json(matchingBooks);
+      return res.status(200).json(matchingBooks);
+    })
+    .catch(error => console.log(error));
   });
+  
   
 
 // Get book review
